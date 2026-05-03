0acfeaa8df1ac30169f5b8d2ef62b9a8e9d6ee28
thiagomw <thiagoribeiromw@gmail.com>
2026-05-03 02:44:48 +0000

    edit:  atualizado código com função e ajustado link do github na lista de hosts

--- cenario-01/check_hosts.py
+++ cenario-01/check_hosts.py
@@ -5,24 +5,32 @@ import os
 hosts = [
     {"name": "Google DNS", "host": "8.8.8.8", "port": 53},
     {"name": "Cloudflare", "host": "1.1.1.1", "port": 53},
-    {"name": "GitHub", "host": "github.csom", "port": 443},
+    {"name": "GitHub", "host": "github.com", "port": 443},
     {"name": "Oracle Cloud", "host": "oracle.com", "port": 443},
 ]
 
-data_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-resultados = []
 
-with open ("/home/ubuntu/devops-lab/cenario-01/check_report.log", "a") as f:
-    for host in hosts:
-        try:
-            conexao = socket.create_connection((host["host"], host["port"]), timeout=3)
-            conexao.close()
-            status, icone = "UP", "✅"
-        except Exception:
-            status, icone = "DOWN", "❌"
 
-        resultado = f"[{data_hora}] {icone} {host['name']}:{host['port']} — {status}"
-        resultados.append(resultado)
-        print(resultado)
+def check_hosts(hosts):
 
-        f.write(f"{resultado}\n")
+    data_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    resultados = []
+
+    with open ("/home/ubuntu/devops-lab/cenario-01/check_report.log", "a") as f:
+        for host in hosts:
+            try:
+                conexao = socket.create_connection((host["host"], host["port"]), timeout=3)
+                conexao.close()
+                status, icone = "UP", "✅"
+            except Exception:
+                status, icone = "DOWN", "❌"
+
+            resultado = f"[{data_hora}] {icone} {host['name']}:{host['port']} — {status}"
+            resultados.append(resultado)
+            print(resultado)
+
+            f.write(f"{resultado}\n")
+
+if __name__ == "__main__":
+
+    check_hosts(hosts)
